85960cda9c869be467b912808ef812f8d04a36d0
acoburn <acoburn@users.noreply.github.com>
2021-07-21 09:53:51 +0000

    deploy: 8ea24ee94239a229275b63fc3f9255f5f788a641

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Requires
-# python3 -m pip install --upgrade pip
-# python3 -m pip install --upgrade bikeshed
-# bikeshed update (to get the latest autolinking data)
-
-for bsdoc in ./index.bs ./primer/index.bs; do bikeshed spec $bsdoc; done
-for diagram in primer/*.mmd; do docker run --rm -v "$PWD:/data" minlag/mermaid-cli -i /data/$diagram; done
