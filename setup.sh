43b03724e786819815e28251e6c59d28618fbb05
Matthieu Bosquet <matthieu@cognithive.com>
2021-07-21 10:56:47 +0100

    Code style of CI

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@
 # python3 -m pip install --upgrade bikeshed
 # bikeshed update (to get the latest autolinking data)
 
-for bsdoc in ./index.bs ./primer/index.bs; do bikeshed spec $bsdoc; done
+for bsdoc in ./*.bs ./**/*.bs; do bikeshed spec $bsdoc; done
 for diagram in primer/*.mmd; do docker run --rm -v "$PWD:/data" minlag/mermaid-cli -i /data/$diagram; done
