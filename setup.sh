2e55e080de77fc8553f77719a06f28a616203568
Christoph Braun <christoph.braun@protonmail.com>
2026-04-21 13:02:30 +0200

    init

--- setup.sh
+++ setup.sh
@@ -5,5 +5,11 @@
 # pipx install bikeshed
 # bikeshed update (to get the latest autolinking data)
 
-for bsdoc in ./*.bs ./**/*.bs; do bikeshed spec $bsdoc; done
-for diagram in primer/*.mmd; do docker run --rm -v "$PWD:/data" minlag/mermaid-cli -i /data/$diagram; done
+# Enable recursive globbing for **
+shopt -s globstar
+
+# 1. Generate ALL diagrams
+for diagram in ./*.mmd ./**/*.mmd; do [ -f "$diagram" ] && docker run --rm -u $(id -u):$(id -g) -v "$PWD:/data" minlag/mermaid-cli -i "/data/$diagram"; done
+
+# 2. Build ALL specs
+for bsdoc in ./*.bs ./**/*.bs; do [ -f "$bsdoc" ] && bikeshed spec "$bsdoc"; done
---
+++ snapshot-gat/README.md
@@ -0,0 +1,6 @@
+# Solid-OIDC: Global Access Token (GAT) Snapshot
+
+Addresses https://github.com/solid/solid-oidc/issues/250 and captures the authentication flow that all current server and client implementations support (as of 2026-04-21).
+
+This specification version is a snapshot of Solid-OIDC, where a Client uses a global DPOP-bound access token issued by an OpenID Provider to authenticate at an Resource Server.
+
